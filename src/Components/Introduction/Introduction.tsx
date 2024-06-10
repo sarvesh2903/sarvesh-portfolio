ea189fb7b0728cb21eab2a7251253526d9226134
sarveshpatiljio <sarvesh1.patil@ril.com>
2024-06-10 11:46:41 +0530

    intro header skills

--- src/Components/Introduction/Introduction.tsx
+++ src/Components/Introduction/Introduction.tsx
@@ -11,17 +11,10 @@ export const Introduction = () => {
         </div>
         <div className="introduction__text">
           <div className="introduction__start">Hello My Name is</div>
-          <h2 className="introduction__heading">Sarvesh Patil</h2>
+          <h2 className="introduction__heading">Lorem, ipsum.</h2>
           <div className="introduction__description">
             I am a Web developer. I have a hands on experience on Angular and
-            react . Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Hic nobis omnis fugit dolorem itaque enim atque cum iusto corrupti
-            eaque vel, ex rerum. Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Optio fuga quam adipisci, dolores unde quasi quod
-            vero reprehenderit odit quidem repellat repellendus ab consectetur
-            pariatur, natus explicabo sapiente eveniet voluptate. Ex neque
-            blanditiis cupiditate voluptatibus consequatur atque est quis
-            impedit?
+            react .
           </div>
         </div>
       </div>
--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -27,14 +27,24 @@ export const Skills = () => {
     },
   ];
   return (
-    <section className="skills">
-      {skillsList.map((skillBoxProps) => (
-        <SkillBox
-          skillHeader={skillBoxProps.skillHeader}
-          SkillIcon={skillBoxProps.SkillIcon}
-          subSkills={skillBoxProps.subSkills}
-        />
-      ))}
+    <section>
+      <h1>Skills Overview</h1>
+      <h4>
+        I have a one and half year of experience of building and managing web
+        applications that faces for than 5 lakhs of monthly active users. Lorem
+        ipsum, dolor sit amet consectetur adipisicing elit. Dolor nisi inventore
+        harum voluptates deserunt commodi dicta accusantium, consequatur,
+        eveniet tenetur ab eius atque, quam adipisci.
+      </h4>
+      <div className="skills">
+        {skillsList.map((skillBoxProps) => (
+          <SkillBox
+            skillHeader={skillBoxProps.skillHeader}
+            SkillIcon={skillBoxProps.SkillIcon}
+            subSkills={skillBoxProps.subSkills}
+          />
+        ))}
+      </div>
     </section>
   );
 };
@@ -43,12 +53,15 @@ const SkillBox = ({ skillHeader, SkillIcon, subSkills }: ISkillboxProps) => {
   return (
     <div className="skillbox">
       <h2>{skillHeader}</h2>
+
       <div className="skillbox__icon">
         <SkillIcon />
       </div>
-      {subSkills.map((skill) => (
-        <div className="skillbox__skill">{skill}</div>
-      ))}
+      <div className="skillbox__skills">
+        {subSkills.map((skill) => (
+          <div className="skillbox__skill">{skill}</div>
+        ))}
+      </div>
     </div>
   );
 };
